06ebea9ec4e268e7b1aeed5731493a14d0f44b89
abhipin <73682089+abhipin@users.noreply.github.com>
2021-09-27 16:16:10 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,5 +1,10 @@
+# demonstration
+
 import download
 
+# create a RequestObject
 request = download.RequestObject()
+#set durl to the url that you want to download
 request.durl = "https://www.google.com"
+#call dload() to download
 download.dload(request)
