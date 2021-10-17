fc98761ba43eb6a54e736189987757f7d2c2da26
abhipin <abhinavpin@gmail.com>
2021-10-17 22:08:34 +0530

    Rewrote some code

--- download.py
+++ download.py
@@ -1,9 +1,28 @@
 
 import urllib.request
+from html.parser import HTMLParser
 
 class RequestObject():
     durl = None
     method = "GET"
+
+
+HTMLINDN = 0
+
+class HTMLParse(HTMLParser):
+    global HTMLINDN
+    def handle_starttag(self, tag, attrs):
+        self.HTMLINDN += 1
+
+    def handle_endtag(self, tag):
+        self.HTMLINDN -= 1
+
+    def handle_data(self, data):
+        print("yes")
+        print(('    '*self.HTMLINDN)+str(list(data).insert(0, '<').append('>')))
+
+class ModuleSettings():
+    showprg = False
 
 # Errors
 
@@ -19,7 +38,13 @@ def dload(reqobj, fln):
             rOb = urllib.request.Request(reqobj.durl)
             resobj = urllib.request.urlopen(rOb)
             with open(fln, 'wb') as fl:
-                fl.write(resobj.read())
+                resread = resobj.read()
+                extension = fln.split('.')[1]
+                if extension == "html":
+                    print('yes')
+                    parser = HTMLParse()
+                    parser.feed(str(resobj.read())[17:])
+                fl.write(resread)
         else:
             raise URLEmpty("Given URL is empty")
     else:
